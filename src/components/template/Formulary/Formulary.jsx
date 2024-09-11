ce6e6175dc2622033c1c05c17a3f58c97e31f4ed
Cristiane Nazareth <cristianenazareth24@gmail.com>
2024-09-11 19:46:28 -0300

    fetch update

--- src/components/template/Formulary/Formulary.jsx
+++ src/components/template/Formulary/Formulary.jsx
@@ -1,40 +1,81 @@
 import React from "react";
+import { useState } from "react";
 import './Formulary.css';
 import Header from "../Header/Header";
 import Button from "../Button/Button";
+import VideoList from "../VideoList/VideoList"
 
 const Formulary = () => {
-  return (
 
-    <div className="content">
-      <form id="addVideo" className="formularyVideo" method="post">
+  const [title, setTitle] = useState('');
+  const [url, setUrl] = useState('');
+  const [duration, setDuration] = useState('');
 
-        <Header />
+  const submitVideo = async (e) => {
+    e.preventDefault();
 
-        <fieldset className="area">
-          <legend>Dados do video</legend>
-          <div className="field">
-            <label htmlFor="fieldName">Nome: </label>
-            <input className="fieldName" type="text" />
-          </div>
-          <div className="field">
-            <label htmlFor="fieldVideoUrl" aria-placeholder="Insira a url do seu video">Insira a url do seu video: </label>
-            <input type="text" id="fieldVideoUrl" className="fieldVideoUrl" placeholder="Compatível com youtub, vimeo e dailymontion" pattern="https?://.+"></input>
-          </div>
-          <div className="field">
-            <label htmlFor="fieldDuration">Duração: </label>
-            <input type="number" className="fieldDuration"></input>
-          </div>
-        </fieldset>
-        <Button />
-      </form>
+    const video = { title, url, duration };
 
-    </div>
-  )
-}
+    try {
+      const response = await fetch('http://localhost:3333/videos',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(video),
+        })
+
+      if (response.ok) {
+        const newVideo = await response.json();
+        console.log('Vídeo adicionado:', newVideo);
+        setTitle('');
+        setUrl('');
+        setDuration('');
+
+      } else {
+        console.error('Erro ao adicionar vídeo.');
+      }
+    } catch (error) {
+      console.error('Erro na requisição:', error);
+
+    }
+  }
+
+    return (
+
+      <div className="content">
+        <form id="addVideo" className="formularyVideo" method="post" onSubmit={submitVideo}>
+
+          <Header />
+
+          <fieldset className="area">
+            <legend>Dados do video</legend>
+            <div className="field">
+              <label htmlFor="fieldTitle">Título: </label>
+              <input className="fieldTitle" type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+            </div>
+            <div className="field">
+              <label htmlFor="fieldVideoUrl" aria-placeholder="Insira a url do seu video">Insira a url do seu video: </label>
+              <input type="text" id="fieldVideoUrl" className="fieldVideoUrl" placeholder="Compatível com youtub, vimeo e dailymontion" pattern="https?://.+" value={url} onChange={(e) => setUrl(e.target.value)}></input>
+            </div>
+            <div className="field">
+              <label htmlFor="fieldDuration">Duração: </label>
+              <input type="number" className="fieldDuration" value={duration} onChange={(e) => setDuration(e.target.value)}></input>
+            </div>
+          </fieldset>
+          <Button />
+        </form>
+
+        <VideoList />
+
+      </div>
+    )
+
+  };
 
 
-export default Formulary;
+  export default Formulary;
 
 
 
---
+++ src/components/template/VideoList/VideoList.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import './VideoList.css';
+
+
+const VideoList = () => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      try {
+        const response = await fetch('http://localhost:3333/videos');
+        const data = await response.json();
+        setVideos(data);
+      } catch (error) {
+        console.error('Erro ao tentar buscar videos', error);
+      }
+    }
+    fetchVideos();
+  }, []);
+
+
+  return (
+
+    <div>
+      <h2>Lista de Videos</h2>
+
+      <ul>
+        {videos.map((video) => (
+          <li key={video.id}>
+            <strong>Título:</strong> {video.title} <br />
+            <strong>URL:</strong> {video.url} <br />
+            <strong>Duração:</strong> {video.duration}
+          </li>
+
+        ))}
+
+      </ul>
+    </div>
+  )
+}
+
+export default VideoList;
